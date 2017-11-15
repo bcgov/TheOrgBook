e4246cda107a75d9b3ddef2841d7ddd759285cb2
Wade Barnes <wade.barnes@shaw.ca>
2017-11-15 14:21:03 -0800

    Update the Solr schema to match the new schema.

--- tob-api/buildSolrSchema.sh
+++ tob-api/buildSolrSchema.sh
@@ -3,6 +3,13 @@ SCRIPT_DIR=$(dirname $0)
 MANAGE_CMD=${SCRIPT_DIR}/runManageCmd.sh
 OUTPUT_DIR=${SCRIPT_DIR}/../tob-solr/cores/the_org_book/conf
 
+# Fake the Haystack configuration script into hooking up the solr backend
+# Required for the Solr configuration to be updated.
+export SOLR_SERVICE_NAME=django
+export SOLR_CORE_NAME=the_org_book
+export DJANGO_SERVICE_HOST=localhost
+export DJANGO_SERVICE_PORT=80
+
 # ==============================================================================================================================
 usage () {
   echo "========================================================================================"
