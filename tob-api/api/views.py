ad0aec5e9e539b5d274a8e9ee45d86ef5fcc9120
Nicholas Rempel <nbrempel@gmail.com>
2017-11-21 15:53:37 -0800

    fix use of random

--- tob-api/api/views.py
+++ tob-api/api/views.py
@@ -1125,7 +1125,7 @@ class bcovrinStoreClaim():
     pool = NodePool(
         # Hack to use different pool names. Agent lib doesn't support
         # reopening existing pool config...
-        'theorgbook' + str(random()),
+        'theorgbook' + str(random.random()),
         os.path.abspath('./api/genesis_txn'))
     await pool.open()
     orgbook = OrgBookAgent(
