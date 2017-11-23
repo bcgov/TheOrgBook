ed1bca6377db40959aa900c03d48058d4ebfdf6f
Stephen Curran <swcurran@gmail.com>
2017-11-22 16:26:06 -0800

    Tweaks to readme and scripts. Also removed unnecessary exclaimation mark.

--- openshift/loadData.sh
+++ openshift/loadData.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+usage() { #Usage function
+  cat <<-EOF
+  Tool to load data from the APISpec/TestData folder into the app
+
+  Usage: ./loadData.sh [ -h -x -e <DataLoadEnv> ]
+
+  OPTIONS:
+  ========
+    -h prints the usage for the script
+    -x run the script in debug mode to see what's happening
+    -e <server> load data into the specified server (default: local, Options: local/dev/test/prod/<URL>)
+
+  Update settings.sh and settings.local.sh files to set defaults
+
+EOF
+exit
+}
+
 # Set project and local environment variables
 if [ -f settings.sh ]; then
   . settings.sh
 fi
 
-#Usage function
-function USAGE {
-  echo -e "Usage: $0 [ -h -g -e <DataLoadEnv> ]"
-	echo -e Where:
-	echo -e "-h prints the usage for the script"
-  echo -e "-g run the script - go. Must be specified for the script to be executed"
-  echo -e "-e <server> load data into the specified server (default: ${LOAD_DATA_SERVER}, Options: local/dev/test/prod/URL)"
-  exit 1
-}
+# Script-specific variables to be set
 
-# Process the command line arguments
 # In case you wanted to check what variables were passed
 # echo "flags = $*"
 while getopts e:gh FLAG; do
   case $FLAG in
     e ) LOAD_DATA_SERVER=$OPTARG ;;
-    g ) RUN_SCRIPT="yes" ;;
+    x ) export DEBUG="${YES}" ;;
     h ) USAGE ;;
     \? ) #unrecognized option - show help
       echo -e \\n"Invalid script option: -${OPTARG}"\\n
@@ -34,13 +43,12 @@ done
 shift $((OPTIND-1))
 # echo Remaining arguments: $@
 
-if [ "${RUN_SCRIPT}" != "yes" ]; then
-  echo -e \\n"The -g parameter was not specified - not running script. Exiting..."\\n
-  USAGE
+if [ ! -z "${DEBUG}" ]; then
+  set -x
 fi
 
 # Load Test Data
 echo -e "Loading data into TheOrgBook Server: ${LOAD_DATA_SERVER}"
-pushd ../APISpec/TestData
+pushd ../APISpec/TestData >/dev/null
 ./load-all.sh ${LOAD_DATA_SERVER}
-popd
+popd >/dev/null
