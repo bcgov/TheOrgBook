12b1a034f520bade504a03624b0c4b3c387121df
Stephen Curran <swcurran@gmail.com>
2017-12-22 11:27:22 -0800

    Change settings from arrays to lists because bash

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -4,18 +4,18 @@ export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export -a skip_git_overrides=("schema-spy-build.json" "solr-base-build.json")
+export -a skip_git_overrides="schema-spy-build.json solr-base-build.json"
 export GIT_URI="https://github.com/bcgov/TheOrgBook.git"
 export GIT_REF="master"
 
 # The project components
-export -a components=("tob-db" "tob-solr" "tob-api" "tob-web")
+export components="tob-db tob-solr tob-api tob-web"
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
-export -a builds=("nginx-runtime" "angular-builder")
+export builds="nginx-runtime angular-builder"
 
 # The images to be tagged after build
-export -a images=("angular-on-nginx" "django" "solr" "schema-spy")
+export images="angular-on-nginx django solr schema-spy"
 
 # The routes for the project
-export -a routes=("angular-on-nginx" "django" "solr" "schema-spy")
+export routes="angular-on-nginx django solr schema-spy"
