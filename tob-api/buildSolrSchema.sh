7ec832aa4390cd203d35df80398261fe54bdb42b
Wade Barnes <wade.barnes@shaw.ca>
2018-01-21 14:19:44 -0800

    Update management scripts.

--- tob-api/buildSolrSchema.sh
+++ tob-api/buildSolrSchema.sh
@@ -3,29 +3,53 @@ SCRIPT_DIR=$(dirname $0)
 MANAGE_CMD=${SCRIPT_DIR}/runManageCmd.sh
 OUTPUT_DIR=${SCRIPT_DIR}/../tob-solr/cores/the_org_book/conf
 
-# Fake the Haystack configuration script into hooking up the solr backend
-# Required for the Solr configuration to be updated.
-export SOLR_SERVICE_NAME=django
-export SOLR_CORE_NAME=the_org_book
-export DJANGO_SERVICE_HOST=localhost
-export DJANGO_SERVICE_PORT=80
-
 # ==============================================================================================================================
-usage () {
-  echo "========================================================================================"
-  echo "Builds the schema and configuration file(s) for Solr, based on your project's"
-  echo "Haystack/Solr configuration."
-  echo "The configuration files will be output to, ${OUTPUT_DIR}."
-  echo "----------------------------------------------------------------------------------------"
-  echo "Usage:"
-  echo
-  echo "${0}"
-  echo "========================================================================================"
-  exit 1
+usage() {
+  cat <<-EOF
+  ===============================================================================================
+  Builds the schema and configuration file(s) for Solr, based on your project's 
+  Haystack/Solr configuration.
+  
+  By default the configuration files will be output to, ${OUTPUT_DIR}.
+  -----------------------------------------------------------------------------------------------
+  Usage:
+    ${0} [ -h -x -s <SolrUrl/> -o <OutputDirectory/>]
+  
+  Options:
+    -h Prints the usage for the script
+    -x Enable debug output
+    -s The URL to the Solar search engine instance
+    -o Output directory.  Default; ${OUTPUT_DIR}
+  
+  Example:
+    ${0}
+  ===============================================================================================
+EOF
+exit
 }
+while getopts s:o:xh FLAG; do
+  case $FLAG in
+    s ) export SOLR_URL=$OPTARG
+      ;;
+    o ) OUTPUT_DIR=$OPTARG
+      ;;
+    x ) export DEBUG=1
+      ;;
+    h ) usage
+      ;;
+    \? ) #unrecognized option - show help
+      echo -e \\n"Invalid script option: -${OPTARG}"\\n
+      usage
+      ;;
+  esac
+done
 
-if [ ! -z "${1}" ]; then
-  usage
+shift $((OPTIND-1))
+
+if [ -z "${SOLR_URL}" ]; then
+  # Fake the Haystack configuration script into hooking up the solr backend
+  # Required for the Solr configuration to be updated.
+  export SOLR_URL=http://localhost:8983/solr/the_org_book
 fi
 # ==============================================================================================================================
 
--- tob-api/updateSearchIndex.sh
+++ tob-api/updateSearchIndex.sh
@@ -3,20 +3,41 @@ SCRIPT_DIR=$(dirname $0)
 MANAGE_CMD=${SCRIPT_DIR}/runManageCmd.sh
 
 # ==============================================================================================================================
-usage () {
-  echo "========================================================================================"
-  echo "Rebuilds the Haystack indexes for the project."
-  echo "----------------------------------------------------------------------------------------"
-  echo "Usage:"
-  echo
-  echo "${0}"
-  echo "========================================================================================"
-  exit 1
+usage() {
+  cat <<-EOF
+  ========================================================================================
+  Updates the Haystack indexes for the project.
+  ----------------------------------------------------------------------------------------
+  Usage:
+    ${0} [ -h -x -s <SolrUrl/> ]
+  
+  Options:
+    -h Prints the usage for the script
+    -x Enable debug output
+    -s The URL to the Solar search engine instance 
+  
+  Example:
+    ${0} -s http://localhost:8983/solr/the_org_book  
+  ========================================================================================
+EOF
+exit
 }
+while getopts s:xh FLAG; do
+  case $FLAG in
+    s ) export SOLR_URL=$OPTARG
+      ;;
+    x ) export DEBUG=1
+      ;;
+    h ) usage
+      ;;
+    \? ) #unrecognized option - show help
+      echo -e \\n"Invalid script option: -${OPTARG}"\\n
+      usage
+      ;;
+  esac
+done
 
-if [ ! -z "${1}" ]; then
-  usage
-fi
+shift $((OPTIND-1))
 # ==============================================================================================================================
 
 ${MANAGE_CMD} update_index
