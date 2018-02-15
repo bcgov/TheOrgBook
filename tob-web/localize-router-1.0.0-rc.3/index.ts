aa5aa150aad53db00ebfef66755bfae361f779bd
Andrew Whitehead <andrew@1crm.com>
2018-02-15 11:37:44 -0800

    pull localize-router from custom git repo

--- tob-web/localize-router-1.0.0-rc.3/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/localize-router.module';
-export * from './src/localize-router.parser';
-export * from './src/localize-router.service';
-export * from './src/localize-router.pipe';
-export * from './src/localize-router.config';
-export * from './src/localize-router-config-loader';
