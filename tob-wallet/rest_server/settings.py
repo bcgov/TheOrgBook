7023f1dbe4d75c9c1b3e32f8e6dd1c88c5883e5e
Wade Barnes <wade.barnes@shaw.ca>
2018-04-17 09:25:31 -0700

    Fix static file configuration.

--- tob-wallet/rest_server/settings.py
+++ tob-wallet/rest_server/settings.py
@@ -20,7 +20,7 @@ except:
      import database
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'request_logging.middleware.LoggingMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rest_server.urls'
@@ -138,6 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure the JWTs to expire after 1 hour, and allow users to refresh near-expiration tokens
 # comment out for DRF tokens
