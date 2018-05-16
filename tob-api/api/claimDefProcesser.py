7e72668f36df01cc538a71287b48a5151b5b7495
Nicholas Rempel <nbrempel@gmail.com>
2018-05-15 18:27:12 -0700

    make changes required to successfully store a credential

--- tob-api/api/claimDefProcesser.py
+++ tob-api/api/claimDefProcesser.py
@@ -5,37 +5,38 @@ from api.indy.claimDefParser import ClaimDefParser
 import logging
 from api.indy import eventloop
 
+
 class ClaimDefProcesser(object):
-  """
-  Parses and processes a claim definition.
-  """
+    """
+    Parses and processes a claim definition.
+    """
 
-  def __init__(self, claimDef) -> None:
-    self.__logger = logging.getLogger(__name__)
-    self.__claimDefParser = ClaimDefParser(claimDef)
+    def __init__(self, claimDef) -> None:
+        self.__logger = logging.getLogger(__name__)
+        self.__claimDefParser = ClaimDefParser(claimDef)
 
-  async def __StoreClaimRequest(self):
-    self.__logger.debug(">>> Storing claim request ...")
-    async with Holder() as holder:
-      claim_request = await holder.store_claim_req(
-        self.__claimDefParser.claimOffer,
-        self.__claimDefParser.claimDefinition
-      )
-    self.__logger.debug("<<< Storing claim request.")
-    return claim_request
+    async def __StoreClaimRequest(self):
+        self.__logger.debug(">>> Storing claim request ...")
+        async with Holder() as holder:
+            (credential_request, credential_request_metadata_json) = await holder.create_cred_req(
+                self.__claimDefParser.claimOffer,
+                self.__claimDefParser.claimDefinition
+            )
+        self.__logger.debug("<<< Storing claim request.")
+        return (credential_request, json.loads(credential_request_metadata_json))
 
-  async def __GenerateRequest(self):
-    self.__logger.debug("Generating claim request ...")
-    claim_request = await self.__StoreClaimRequest()
+    async def __GenerateRequest(self):
+        self.__logger.debug("Generating claim request ...")
+        (credential_request, credential_request_metadata) = await self.__StoreClaimRequest()
 
-    self.__logger.debug(
-      "\n============================================================================\n" +
-      "Claim request generated:\n" +
-      "----------------------------------------------------------------------------\n" +
-      "{0}\n".format(json.dumps(json.loads(claim_request), indent=2)) +
-      "============================================================================\n")
+        self.__logger.debug(
+            "\n============================================================================\n" +
+            "Claim request generated:\n" +
+            "----------------------------------------------------------------------------\n" +
+            "{0}\n".format(json.dumps(json.loads(credential_request), indent=2)) +
+            "============================================================================\n")
 
-    return claim_request
+        return (credential_request, credential_request_metadata)
 
-  def GenerateClaimRequest(self):
-    return eventloop.do(self.__GenerateRequest())
+    def GenerateClaimRequest(self):
+        return eventloop.do(self.__GenerateRequest())
--- tob-api/api/indy/claimParser.py
+++ tob-api/api/indy/claimParser.py
@@ -12,37 +12,46 @@ class ClaimParser(object):
     """
     Parses a generic claim.
     """
+
     def __init__(self, claim: str) -> None:
-      self.__logger = logging.getLogger(__name__)
-      self.__orgData = claim
-      self.__parse()
+        self.__logger = logging.getLogger(__name__)
+        self.__orgData = claim
+        self.__parse()
 
     def __parse(self):
-      self.__logger.debug("Parsing claim ...")
-      data = json.loads(self.__orgData)
-      self.__claim_type = data["claim_type"]
-      self.__claim = data["claim_data"]
-      self.__issuer_did = data["claim_data"]["issuer_did"]
+        self.__logger.debug(
+            "\n============================================================================\n" +
+            "Parsing claim:\n" +
+            "----------------------------------------------------------------------------\n" +
+            "{0}\n".format(json.dumps(json.loads(self.__orgData), indent=2)) +
+            "============================================================================\n")
 
-      # # Get schema from ledger
-      # # Once we upgrade to later version of
-      # try:
-      #   resp = requests.get('{}/ledger/domain/{}'.format(
-      #       LEDGER_URL,
-      #       self.__claim['schema_seq_no']
-      #   ))
-      #   self.__schema = resp.json()
-      # except:
-      #   self.__schema = None
-    
+        data = json.loads(self.__orgData)
+        self.__claim_type = data["claim_type"]
+        self.__claim = data["claim_data"]
+        self.__issuer_did = data["issuer_did"]
+        self.__cred_def = data["cred_def"]
+        self.__cred_req_metadata = data["cred_req_metadata"]
+
+        # # Get schema from ledger
+        # # Once we upgrade to later version of
+        # try:
+        #   resp = requests.get('{}/ledger/domain/{}'.format(
+        #       LEDGER_URL,
+        #       self.__claim['schema_seq_no']
+        #   ))
+        #   self.__schema = resp.json()
+        # except:
+        #   self.__schema = None
+
     def getField(self, field):
-      value = None
-      try:
-        value = self.__claim["values"][field][0]
-      except:
-        pass
+        value = None
+        try:
+            value = self.__claim["values"][field]['raw']
+        except:
+            pass
 
-      return value
+        return value
 
     @property
     def schemaName(self) -> str:
@@ -55,6 +64,14 @@ class ClaimParser(object):
     @property
     def issuerDid(self) -> str:
         return self.__issuer_did
+
+    @property
+    def credDef(self) -> str:
+        return self.__cred_def
+
+    @property
+    def credDefMeta(self) -> str:
+        return self.__cred_req_metadata
 
     @property
     def json(self) -> str:
