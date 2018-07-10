9a2b111f772a4c82a31ed44e3e48dd51a47ccdb9
Wade Barnes <wade.barnes@shaw.ca>
2018-07-10 13:24:57 -0700

    #338 - OpenShift configuration for TheOrgBook-BC

---
+++ openshift/settings.bc-tob.sh
@@ -0,0 +1,4 @@
+# Setting for the TheOrgBook-BC environments.
+# Uses the existing `devex-von-tools` environment for builds and images
+export TOOLS="devex-von-tools"
+export PROJECT_NAMESPACE="devex-von-bc-tob"
