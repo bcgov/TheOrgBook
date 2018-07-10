60f44b0f644849f7128bf322b189cd2f54eadd85
Wade Barnes <wade.barnes@shaw.ca>
2018-07-10 13:48:07 -0700

    #338 - Update namespace configuration

--- openshift/settings.bc-tob.sh
+++ openshift/settings.bc-tob.sh
@@ -2,3 +2,6 @@
 # Uses the existing `devex-von-tools` environment for builds and images
 export TOOLS="devex-von-tools"
 export PROJECT_NAMESPACE="devex-von-bc-tob"
+
+# The project components
+export components="tob-db tob-solr tob-api tob-web tob-wallet"
