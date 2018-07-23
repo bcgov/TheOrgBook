f41b9ebaa5bebf912a10a82b300865bfbb9d17fa
Stephen Curran <swcurran@gmail.com>
2018-07-23 10:37:21 -0700

    #355 Added V2 Test Data and generate script; tweaked readme

--- APISpec/TestData/genV2creds.sh
+++ APISpec/TestData/genV2creds.sh
@@ -6,9 +6,9 @@ usage () {
   echo "----------------------------------------------------------------------------------------"
   echo "Usage:"
   echo
-  echo "${0} <excelFile> <claims tab>"
+  echo "${0} <excelFile>"
   echo
-  echo "Example: ${0} TOBOntClaims.xlsm OntClaims.csv"
+  echo "Example: ${0} TOBV2DataFakeRestaurentsBusinessLicences.xlsx"
   echo
   echo "========================================================================================"
   exit 1
@@ -24,4 +24,4 @@ fi
 # Python 2 is installed - assumes it is /usr/bin/python2 - see top if xls2json.py file
 
 echo Calling xls2json script on file: $1
-./xls2json.py --csv $2 $1
+./xls2json.py $1
