9abd497d08a571f6c9c22424f014d30c20779de7
Andrew Whitehead <andrew@1crm.com>
2018-08-23 17:05:29 -0700

    disable indy functions in old API

--- tob-api/tob_api/settings.py
+++ tob-api/tob_api/settings.py
@@ -124,7 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"
+    },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
     },
