fbe54c1fcc3e07c2e60810b543c8a9a5f1b78798
Andrew Whitehead <andrew@1crm.com>
2018-08-30 14:01:04 -0700

    add REST method for debugging API authentication

--- tob-api/tob_anchor/urls.py
+++ tob-api/tob_anchor/urls.py
@@ -7,6 +7,8 @@ def get_routes():
         ("POST", "/api/v2/indy/generate-credential-request", views.generate_credential_request, {}),
         ("POST", "/api/v2/indy/store-credential", views.store_credential, {}),
         ("POST", "/api/v2/indy/register-issuer", views.register_issuer, {}),
+        ("GET", "/api/v2/indy/debug", views.reqInfo, {}),  # FIXME - REMOVE IN PROD
+        ("GET", "/api/v2/debug", views.reqInfo, {}),  # FIXME - REMOVE IN PROD
         ("GET", "/api/v2/indy/status", views.status, {}),
         ("GET", "/api/v2/credential/{id}/verify", views.verify_credential, {}),
     )]
