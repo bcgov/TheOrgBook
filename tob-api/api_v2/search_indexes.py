1116b2f259ff9d2ea0d25a3a19328044f0c7c39e
Andrew Whitehead <andrew@1crm.com>
2018-09-06 14:41:28 -0700

    optimize queries for index rebuild

--- tob-api/api_v2/search_indexes.py
+++ tob-api/api_v2/search_indexes.py
@@ -1,16 +1,21 @@
 # TODO: Figure out how to configure haystack to register indices in
 #       ./indices/<IndexName> instead of this default file...
 
+from itertools import chain
+
 from haystack import indexes
+from django.db.models import Prefetch
 from django.utils import timezone
 
+from api_v2.models.Credential import Credential as CredentialModel
+from api_v2.models.Name import Name as NameModel
 from api_v2.models.Topic import Topic as TopicModel
 
 
 class TopicIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
-    autocomplete = indexes.EdgeNgramField()
+    #autocomplete = indexes.EdgeNgramField()
 
     name = indexes.CharField()
     location = indexes.CharField()
@@ -18,17 +23,13 @@ class TopicIndex(indexes.SearchIndex, indexes.Indexable):
 
     @staticmethod
     def prepare_name(obj):
-        names = []
-        for credential in obj.credentials.filter(revoked=False):
-            for name in credential.names.all():
-                names.append(name.text or "")
-
-        return " ".join((names))
+        names = ((name.text for name in cred.names.all()) for cred in obj.nonrevoked)
+        return " ".join(chain.from_iterable(names))
 
     @staticmethod
     def prepare_location(obj):
         locations = []
-        for credential in obj.credentials.filter(revoked=False):
+        for credential in obj.nonrevoked:
             for address in credential.addresses.all():
                 locations.append(address.addressee or "")
                 locations.append(address.civic_address or "")
@@ -39,14 +40,17 @@ class TopicIndex(indexes.SearchIndex, indexes.Indexable):
 
         return " ".join((locations))
 
-    # @staticmethod
-    # def prepare_historical(obj):
-    #     return " ".join((obj.source_id))
-
     def get_model(self):
         return TopicModel
 
     def index_queryset(self, using=None):
+        creds = CredentialModel.objects.filter(revoked=False)
+        names = NameModel.objects.all().only('id', 'credential_id', 'text')
+        prefetch = (
+            Prefetch('credentials', queryset=creds, to_attr='nonrevoked'),
+            Prefetch('nonrevoked__names', queryset=names),
+            'nonrevoked__addresses',
+        )
         return self.get_model().objects.filter(
             create_timestamp__lte=timezone.now()
-        )
+        ).prefetch_related(*prefetch)
