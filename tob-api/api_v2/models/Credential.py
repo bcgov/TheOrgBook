89217b20bdff0ef9cc728b20d4be494bb509b967
Andrew Whitehead <andrew@1crm.com>
2018-09-07 10:31:46 -0700

    add credential_def_id to credential, credential type; update migrations; index credentials by wallet_id

--- tob-api/api_v2/models/Credential.py
+++ tob-api/api_v2/models/Credential.py
@@ -15,7 +15,8 @@ class Credential(Auditable):
     topic = models.ForeignKey("Topic", related_name="credentials")
 
     credential_type = models.ForeignKey(CredentialType, related_name="credentials")
-    wallet_id = models.TextField()
+    wallet_id = models.TextField(db_index=True)
+    credential_def_id = models.TextField(db_index=True, null=True)
 
     effective_date = models.DateTimeField(default=timezone.now)
     revoked = models.BooleanField(db_index=True, default=False)
