7e5e632d0abe5cb92dbb7bfd12e64ae1bbc7c992
Olena Mitovska <mitovskaol@ON34C02760025.local>
2018-09-19 17:19:54 -0400

    Adding some AODA fixed
    Signed-off-by: Olena Mitovska <mitovskaol@ON34C02760025.local>

--- tob-web/src/app/general-data.service.ts
+++ tob-web/src/app/general-data.service.ts
@@ -20,16 +20,12 @@ export class GeneralDataService {
   }
 
   getRequestUrl(path: string) : string {
-<<<<<<< HEAD
-   //let root = (<any>window).testApiUrl || this.apiUrl;
-   let root = 'http://localhost:8081/api/v2/'
-=======
     if(typeof path === 'string' && path.match(/^(\/\/|\w+:\/\/)\w/)) {
       // absolute URL
       return path;
     }
     let root = (<any>window).testApiUrl || this.apiUrl;
->>>>>>> upstream/master
+
     if(root) {
       if(! root.endsWith('/')) root += '/';
       return root + path;
