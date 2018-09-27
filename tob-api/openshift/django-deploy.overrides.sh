9d8933b1230a1c536c7ea0180014a4be9709faca
Wade Barnes <wade.barnes@shaw.ca>
2018-09-27 12:11:31 -0700

    Add support for handling DIDs
    - User can specify the DIDs associated to the wallet(s) at the time of configuration deployment
    - The DIDs are stored with the other wallet credentials (key and seeds).

--- tob-api/openshift/django-deploy.overrides.sh
+++ tob-api/openshift/django-deploy.overrides.sh
@@ -119,9 +119,16 @@ _issuerWalletPrefix="${_walletPrefix}I"
 readParameter "HOLDER_WALLET_SEED - Please provide the holder wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" HOLDER_WALLET_SEED $(generateSeed ${_holderWalletPrefix}) "true"
 readParameter "VERIFIER_WALLET_SEED - Please provide the verifier wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" VERIFIER_WALLET_SEED $(generateSeed ${_verifierWalletPrefix}) "true"
 
-# TheOrgBook does not use a issuer wallet at the moment so just generate one without asking ...
+# TheOrgBook does not use a issuer wallet at the moment so just generate a seed without asking ...
 # readParameter "ISSUER_WALLET_SEED - Please provide the issuer wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" ISSUER_WALLET_SEED $(generateSeed ${_issuerWalletPrefix}) "true"
 writeParameter "ISSUER_WALLET_SEED" $(generateSeed ${_issuerWalletPrefix}) "true"
+
+readParameter "HOLDER_WALLET_DID - Please provide the holder wallet did for the environment.  The default is an empty string:" HOLDER_WALLET_DID "" "true"
+readParameter "VERIFIER_WALLET_DID - Please provide the verifier did seed for the environment.  The default is an empty string:" VERIFIER_WALLET_DID "" "true"
+
+# TheOrgBook does not use a issuer wallet at the moment so just write the DID without asking ...
+# readParameter "ISSUER_WALLET_DID - Please provide the issuer wallet did for the environment.  The default is an empty string:" ISSUER_WALLET_DID "" "true"
+writeParameter "ISSUER_WALLET_DID" "" "true"
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
 echo ${SPECIALDEPLOYPARMS}
