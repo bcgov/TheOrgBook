5a8700155a83dd8bb29409f6e43f00a78b0d13f4
Olena Mitovska <mitovskaol@ON34C02760025.local>
2018-10-29 16:18:43 -0400

    Added timezone override for dates coming in attributes array. Ontario needto be able to force the timezøne to be EST for all displayed dates and not depend on the user local timezone

--- tob-web/src/app/util/date-format.pipe.ts
+++ tob-web/src/app/util/date-format.pipe.ts
@@ -7,9 +7,10 @@ import { DatePipe } from '@angular/common';
 export class DateFormatPipe extends DatePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let format = arguments[1] || 'mediumDate';
+    let timezone = arguments[2] || 'UTC';
     if(format === 'effectiveDate') format = 'mediumDate';
     else if(format === 'effectiveDateTime') format = 'MMM d, y, h:mm a';
     // may want to automatically strip off time if equal to midnight
-    return super.transform(value, format);
+    return super.transform(value, format, timezone);
   }
 }
