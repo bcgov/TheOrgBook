f56356950941e971c595f5801538c7a8e1882de0
Andrew Whitehead <andrew@1crm.com>
2019-01-07 13:59:13 -0800

    add on_delete parameter required by django 2.x

--- tob-api/api_v2/models/CredentialSet.py
+++ tob-api/api_v2/models/CredentialSet.py
@@ -5,10 +5,10 @@ from auditable.models import Auditable
 
 
 class CredentialSet(Auditable):
-    credential_type = models.ForeignKey("CredentialType", related_name="credential_sets")
+    credential_type = models.ForeignKey("CredentialType", related_name="credential_sets", on_delete=models.CASCADE)
     cardinality_hash = models.TextField(db_index=True, null=True)
-    latest_credential = models.ForeignKey("Credential", related_name="+", null=True)
-    topic = models.ForeignKey("Topic", related_name="credential_sets")
+    latest_credential = models.ForeignKey("Credential", related_name="+", null=True, on_delete=models.SET_NULL)
+    topic = models.ForeignKey("Topic", related_name="credential_sets", on_delete=models.CASCADE)
 
     first_effective_date = models.DateTimeField(null=True)
     last_effective_date = models.DateTimeField(null=True)
