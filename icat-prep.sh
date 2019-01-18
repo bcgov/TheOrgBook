9cccc8206a2b9307b28519c820b236b0c84afc18
Ian Costanzo <ian@anon-solutions.ca>
2019-01-17 16:11:21 -0800

    Updated script and instructions for indycat migration

--- icat-prep.sh
+++ icat-prep.sh
@@ -1,3 +1,28 @@
+# This script re-orgs the OrgBook repo in preparation for merge into indy-catalyst
+#
+# Before running this script:
+# - create a new branch (e.g. merge_test) so a to not screw up master
+#
+# Run this script from TheOrgBook root
+#
+# After running this script:
+# - commit into github (into the new branch)
+#
+# In the indy-catalyst repo:
+# - start on a new branch (for safety)
+# - git remote add orgbook https://github.com/bcgov/TheOrgBook.git
+# - git fetch orgbook
+# - git merge orgbook/merge-test (or whatever you called your branch)
+#
+# To build the starter kit (server):
+# - cd to credential-registry/server
+# - ./icat-prep.sh # builds packages for tob-api
+# - cd to starter-kits/credential-registry/server/docker
+# - update manage script (see README files in the above credential-registry/server sub-directories)
+# - ./manage build
+# - ./manage start
+#
+# Note it requires a von network to be already running
 
 mkdir starter-kits
 mkdir starter-kits/credential-registry
@@ -18,9 +43,12 @@ mkdir credential-registry/server/python-indy-api
 
 mv starter-kits/credential-registry/server/tob-api/api_v2 credential-registry/server/django-icat-api/
 mv starter-kits/credential-registry/server/tob-api/api_indy credential-registry/server/python-indy-api/
+# TODO figure out where the tob-web code needs to live
 #mv starter-kits/credential-registry/server/tob-web starter-kits/credential-registry/client/
 
 rm starter-kits/credential-registry/server/Deploy*
+rm starter-kits/credential-registry/server/SonarQube-*
+rm starter-kits/credential-registry/server/Zap-*
 rm -rf starter-kits/credential-registry/server/openshift
 rm -rf starter-kits/credential-registry/server/sonar-runner
 rm -rf starter-kits/credential-registry/server/tob-backup
