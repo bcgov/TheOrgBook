837dcdcea253d2b67b9b4b155f134bace16d12fe
Andrew Whitehead <cywolf@gmail.com>
2019-02-11 11:35:36 -0800

    in attribute formatting, avoid adjustment of date value according to the configured timezone

--- tob-web/src/app/util/date-format.pipe.ts
+++ tob-web/src/app/util/date-format.pipe.ts
@@ -14,10 +14,18 @@ export class DateFormatPipe implements PipeTransform {
 
   transform(value: any, format = 'mediumDate', timezone?: string): any {
     if (value == null || value === '' || value !== value) return null;
-    if(format === 'effectiveDate') format = 'mediumDate';
-    else if(format === 'effectiveDateTime') format = 'MMM d, y, h:mm a';
-    if (! timezone)
+
+    if(format === 'effectiveDate')
+      format = 'mediumDate';
+    else if(format === 'effectiveDateTime')
+      format = 'MMM d, y, h:mm a';
+
+    if(typeof value == 'string' && value.match(/^\d{4}-\d{2}-\d{2}/))
+      // avoid date value changing due to timezone calculation
+      timezone = undefined;
+    else if (! timezone)
       timezone = this._config.getConfig().DISPLAY_TIMEZONE;
+
     return formatDate(value, format, this._locale, timezone);
   }
 }
