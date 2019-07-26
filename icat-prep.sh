b924c4a0b4b745d4a011aebd877f33c248943615
Ian Costanzo <ian@anon-solutions.ca>
2019-07-26 12:12:35 -0700

    tinker with icat migration script
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- icat-prep.sh
+++ icat-prep.sh
@@ -53,14 +53,14 @@ rm starter-kits/credential-registry/server/Deploy*
 rm starter-kits/credential-registry/server/SonarQube-*
 rm starter-kits/credential-registry/server/Zap-*
 
-rm -rf starter-kits/credential-registry/tob-db/openshift
-rm starter-kits/credential-registry/tob-db/Jenkinsfile
-rm -rf starter-kits/credential-registry/tob-api/openshift
-rm starter-kits/credential-registry/tob-api/Jenkinsfile
-rm -rf starter-kits/credential-registry/tob-solr/openshift
-rm starter-kits/credential-registry/tob-solr/Jenkinsfile
-rm -rf starter-kits/credential-registry/tob-wallet/openshift
-rm starter-kits/credential-registry/tob-wallet/Jenkinsfile
+rm -rf starter-kits/credential-registry/server/tob-db/openshift
+rm starter-kits/credential-registry/server/tob-db/Jenkinsfile
+rm -rf starter-kits/credential-registry/server/tob-api/openshift
+rm starter-kits/credential-registry/server/tob-api/Jenkinsfile
+rm -rf starter-kits/credential-registry/server/tob-solr/openshift
+rm starter-kits/credential-registry/server/tob-solr/Jenkinsfile
+rm -rf starter-kits/credential-registry/server/tob-wallet/openshift
+rm starter-kits/credential-registry/server/tob-wallet/Jenkinsfile
 
 rm -rf starter-kits/credential-registry/server/openshift
 rm -rf starter-kits/credential-registry/server/sonar-runner
