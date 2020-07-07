4500a1937db330ef18cc8ec149605e317bc84761
Wade Barnes <wade.barnes@shaw.ca>
2020-07-07 07:35:12 -0700

    Update deleteTopic command and script
    - Update documentation
    - Set topic_id as a required parameter
    - Ensure topic_id is passed as a parameter to the deleteTopic script.
    - Update command registration
    
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- tob-api/deleteTopic.sh
+++ tob-api/deleteTopic.sh
@@ -5,28 +5,36 @@ MANAGE_CMD=${SCRIPT_DIR}/runManageCmd.sh
 # ==============================================================================================================================
 usage() {
   cat <<-EOF
-  ========================================================================================
-  Delete all OrgBook data for the specified Topic.
-  ----------------------------------------------------------------------------------------
-  Usage:
-    ${0} <topic_id> [ -h ]
-  
-  Options:
-    <topic_id> is the subject_id of the Topic to delete, e.g. BC1234567
-    -h Prints the usage for the script
-  ========================================================================================
+
+========================================================================================
+Delete all OrgBook data for the specified Topic.
+----------------------------------------------------------------------------------------
+Usage:
+  ${0} <topic_id> [ -h ]
+
+Options:
+  <topic_id> is the subject_id of the Topic to delete, e.g. BC1234567
+  -h Prints the usage for the script
+========================================================================================
 EOF
-exit
 }
 
 while getopts h FLAG; do
   case $FLAG in
     h ) usage
+        exit
       ;;
   esac
 done
 
 shift $((OPTIND-1))
 # ==============================================================================================================================
+
+_topic_id=${1}
+if [ -z "${_topic_id}" ]; then
+  usage
+  echo -e \\n"deleteTopic; Missing required <topic_id> parameter."\\n
+  exit 1
+fi
 
 ${MANAGE_CMD} delete_topic "$@"
